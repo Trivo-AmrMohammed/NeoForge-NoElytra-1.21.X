e953177719626c25744eb5cd4b4b96517ef9e97c
NewUsername <amralhwid@gmail.com>
2025-08-11 01:31:44 +0300

    Reweorked elytra mod with multi modded elytra disabling and allows operators and creative players to fly with it.

---
+++ src/main/java/net/trivo/trivonoelytra/NoElytraMod.java
@@ -0,0 +1,73 @@
+package net.trivo.trivonoelytra;
+
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.util.profiling.jfr.event.PacketReceivedEvent;
+import net.minecraft.world.item.Items;
+import net.neoforged.bus.api.EventPriority;
+import net.neoforged.neoforge.event.entity.EntityJoinLevelEvent;
+import net.neoforged.neoforge.event.entity.player.PlayerEvent;
+import net.neoforged.neoforge.event.tick.PlayerTickEvent;
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+
+import java.util.Objects;
+
+// The value here should match an entry in the META-INF/neoforge.mods.toml file
+@Mod(NoElytraMod.MODID)
+public class NoElytraMod {
+    // Define mod id in a common place for everything to reference
+    public static final String MODID = "trivonoelytra";
+    // Directly reference a slf4j logger
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    // The constructor for the mod class is the first code that is run when your mod is loaded.
+    // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
+    public NoElytraMod(IEventBus modEventBus, ModContainer modContainer) {
+
+        // Register ourselves for server and other game events we are interested in.
+        // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
+        // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
+        NeoForge.EVENT_BUS.register(this);
+
+        // Register our mod's ModConfigSpec so that FML can create and load the config file for us
+        modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+    @SubscribeEvent
+    public void onElytraFlight(PlayerTickEvent.Post event) {
+        if (!(event.getEntity() instanceof ServerPlayer serverPlayer)) return;
+        if (serverPlayer.isCreative()) return;
+        if (serverPlayer.hasPermissions(2)) return;
+
+        if (serverPlayer.isFallFlying()) {
+            serverPlayer.stopFallFlying();
+        }
+    }
+
+    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+
+    }
+
+    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    @EventBusSubscriber(modid = NoElytraMod.MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    static class ClientModEvents {
+        @SubscribeEvent
+        static void onClientSetup(FMLClientSetupEvent event) {
+
+        }
+    }
+}
